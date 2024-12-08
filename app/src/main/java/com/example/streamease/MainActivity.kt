437c987c22afc03abbc919a2fbfcf26c31443e65
Sury <24je0706@iitism.ac.in>
2024-12-08 13:51:58 +0530

    preparing for Recycle Viewer

--- app/src/main/java/com/example/streamease/MainActivity.kt
+++ app/src/main/java/com/example/streamease/MainActivity.kt
@@ -4,31 +4,41 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.streamease.Models.PageData
+import com.example.streamease.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private val APIKEY:String = "ugpXVoRZqu4YZYA4pIRXwVYP8Mgyn5O3aZBYkTC2Z5CFn7tgZCz4M5ml"
-
+    private lateinit var binding:ActivityMainBinding;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-        fetchData()
+
+        fetchData(binding)
     }
 
-    private fun fetchData() {
+   private fun fetchData(binding: ActivityMainBinding) {
         RetrofitClient.instance?.api?.getPopular(APIKEY)?.enqueue(object : Callback<PageData> {
             override fun onResponse(p0: Call<PageData>, p1: Response<PageData>) {
-                val tV:TextView = findViewById(R.id.tV)
-                tV.text = p1.body()?.url
+
+                binding.PageNo.text = buildString {
+                    append("Page No.:")
+                    append(p1.body()?.page.toString())
+                }
+                binding.totalRES.text = buildString {
+                    append("TOTAL RESULTS:")
+                    append(p1.body()?.total_results.toString())
+                }
             }
 
             override fun onFailure(p0: Call<PageData>, p1: Throwable) {
-                val tV:TextView = findViewById(R.id.tV)
+                val tV:TextView = binding.PageNo;
                 tV.text = buildString {
                     append("Error")
                     append(p1.message)
