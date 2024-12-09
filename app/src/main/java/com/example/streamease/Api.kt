d83f000301da9e2f5c14ddda0c9844b828a138de
Sury <24je0706@iitism.ac.in>
2024-12-09 15:16:44 +0530

    Pagenation done

--- app/src/main/java/com/example/streamease/Api.kt
+++ app/src/main/java/com/example/streamease/Api.kt
@@ -4,12 +4,16 @@ import com.example.streamease.Models.PageData
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Query
 
 interface Api  {
     @GET("popular")
     fun getPopular(
         @Header("Authorization")
-        Credential:String
-
+        Credential:String,
+        @Query("page")
+        page:Int,
+        @Query("per_page")
+        per_page:Int
     ): Call<PageData>
 }
--- app/src/main/java/com/example/streamease/MainActivity.kt
+++ app/src/main/java/com/example/streamease/MainActivity.kt
@@ -1,43 +1,89 @@
 package com.example.streamease
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.NestedScrollView
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.streamease.Models.PageData
+import com.example.streamease.Models.Video
 import com.example.streamease.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+
 class MainActivity : AppCompatActivity() {
-    private val APIKEY:String = "ugpXVoRZqu4YZYA4pIRXwVYP8Mgyn5O3aZBYkTC2Z5CFn7tgZCz4M5ml"
-    private lateinit var binding:ActivityMainBinding;
-    private lateinit var recycleV:RecyclerView
+    private val APIKEY: String = "ugpXVoRZqu4YZYA4pIRXwVYP8Mgyn5O3aZBYkTC2Z5CFn7tgZCz4M5ml"
+    private lateinit var binding: ActivityMainBinding;
+    private lateinit var recycleV: RecyclerView
+    private var page: Int = 1
+    private var per_page: Int = 20
+    private var total_res:Int= Int.MAX_VALUE
+    private lateinit var nestedScrollView: NestedScrollView
+    private lateinit var linearLayoutManager:LinearLayoutManager
+    private  lateinit var loadingPB:ProgressBar
+    private lateinit var videolist:List<Video>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
-
+        videolist = listOf()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        nestedScrollView = binding.nestedscrollview
         recycleV = binding.recycleview
-        fetchData(binding)
+        linearLayoutManager=LinearLayoutManager(this@MainActivity)
+        loadingPB =binding.idPBLoading
+        fetchData(binding, page, total_res)
+        SetUpPagination(true)
     }
 
-   private fun fetchData(binding: ActivityMainBinding) {
-        RetrofitClient.instance?.api?.getPopular(APIKEY)?.enqueue(object : Callback<PageData> {
+    private fun SetUpPagination(isPaginationAllowed: Boolean) {
+        if (isPaginationAllowed) {
+            nestedScrollView.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
+                // on scroll change we are checking when users scroll as bottom.
+                if (scrollY == v.getChildAt(0).measuredHeight - v.measuredHeight) {
+                    // in this method we are incrementing page number,
+                    // making progress bar visible and calling get data method.
+                    page++
+                    loadingPB.setVisibility(View.VISIBLE)
+                    fetchData(binding,page,total_res)
+                }
+            })
+
+
+        }
+    }
+
+    private fun fetchData(binding: ActivityMainBinding, i: Int, tot: Int) {
+        if (i > tot) {
+            // checking if the page number is greater than limit.
+            // displaying toast message in this case when page>limit.
+            Toast.makeText(this, "That's all the data..", Toast.LENGTH_SHORT).show();
+
+            // hiding our progress bar.
+            loadingPB.setVisibility(View.GONE);
+            return;
+        }
+        RetrofitClient.instance?.api?.getPopular(APIKEY,i,per_page)?.enqueue(object : Callback<PageData> {
             override fun onResponse(p0: Call<PageData>, p1: Response<PageData>) {
-                val videoslist = p1.body()?.videos;
-                val adapter = myAdapter(this@MainActivity,videoslist)
+                for (i in p1.body()?.videos!!){
+                    videolist+=i;
+                }
+                val adapter = myAdapter(this@MainActivity, videolist)
                 recycleV.adapter = adapter
-                recycleV.layoutManager = LinearLayoutManager(this@MainActivity)
+                recycleV.layoutManager = linearLayoutManager
 
                 binding.PageNo.text = buildString {
                     append("Page No.:")
                     append(p1.body()?.page.toString())
                 }
+                this@MainActivity.total_res = p1.body()?.total_results!!
                 binding.totalRES.text = buildString {
                     append("TOTAL RESULTS:")
                     append(p1.body()?.total_results.toString())
@@ -45,7 +91,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onFailure(p0: Call<PageData>, p1: Throwable) {
-                val tV:TextView = binding.PageNo;
+                val tV: TextView = binding.PageNo;
                 tV.text = buildString {
                     append("Error")
                     append(p1.message)
